adfb27aa9f3646510da02ec98ddc4478a2de0345
Divine Charles <officialkb17@gmail.com>
2023-05-26 08:28:51 +0200

    added arrow motion

---
+++ index.js
@@ -0,0 +1,14 @@
+const navToggleBtn = document.querySelector(".nav-toggle-btn");
+const navbar = document.querySelector(".navbar__list");
+const navLinks = document.querySelectorAll(".nav__link");
+
+navToggleBtn.addEventListener("click", toggleNav)
+
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", toggleNav);
+};
+
+function toggleNav () {
+  navToggleBtn.classList.toggle("active");
+  navbar.classList.toggle("active");
+};
